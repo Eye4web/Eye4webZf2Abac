502243eff257e0be9f84ecf059c67567af374f32
nikolajpetersen <np@eye4web.dk>
2014-09-17 11:03:56 +0200

    Assertion plugin manager factory test

---
+++ test/Eye4web/Zf2AbacTest/Factory/Assertion/AssertionPluginManagerFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ZfcRbacTest\Factory;
+
+use Zend\ServiceManager\ServiceManager;
+use Eye4web\Zf2Abac\Factory\Assertion\AssertionPluginManagerFactory;
+
+/**
+ * @covers \Eye4web\Zf2Abac\Factory\Assertion\AssertionPluginManagerFactory
+ */
+class AssertionPluginManagerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFactory()
+    {
+        $serviceManager = new ServiceManager();
+        $serviceManager->setService('Config', [
+            'eye4web_abac' => [
+                'assertion_manager' => [],
+            ]
+        ]);
+
+        $factory       = new AssertionPluginManagerFactory();
+        $pluginManager = $factory->createService($serviceManager);
+
+        $this->assertInstanceOf('Eye4web\Zf2Abac\Assertion\AssertionPluginManager', $pluginManager);
+        $this->assertSame($serviceManager, $pluginManager->getServiceLocator());
+    }
+}
